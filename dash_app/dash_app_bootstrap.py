7f4de677b80f671b2ad8b73e7eb7f45860c7004d
dilaradupont <92064348+dilaradupont@users.noreply.github.com>
2022-01-20 17:38:26 +0000

    Initial commit

---
+++ dash_app/dash_app_bootstrap.py
@@ -0,0 +1,36 @@
+import dash
+from dash import html, dcc
+import plotly.express as px
+import pandas as pd
+import dash_bootstrap_components as dbc
+
+app = dash.Dash(
+    external_stylesheets=[dbc.themes.BOOTSTRAP],
+    meta_tags=[
+        {"name": "viewport", "content": "width=device-width, initial-scale=1"},
+    ],
+)
+
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
